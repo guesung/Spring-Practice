0c7b47d8dd4a09ed48259c32a81fcbaf5be612ab
Kuesung Park <gueit214@naver.com>
2023-10-09 15:09:46 +0900

    feat : member 서버

---
+++ src/main/java/com/example/member/AppConfig.java
@@ -0,0 +1,44 @@
+package com.example.member;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Optional;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    MemberRepository initMember() {
+        MemberRepository repository = new MemberRepository() {
+            @Override
+            public void init() {
+                // 샘플 데이터 등록
+                memberMap.put(1, new Member(1, "홍길동", "hong@korea.com"));
+                memberMap.put(2, new Member(2, "김철수", "kcs@google.com"));
+                memberMap.put(3, new Member(3, "김영희", "kyh@naver.com"));
+            }
+
+            @Override
+            public Optional<Member> findById(Integer id) {
+                return Optional.ofNullable(memberMap.get(id));
+            }
+
+            @Override
+            public Iterable<Member> findAll() {
+                return memberMap.values();
+            }
+
+            @Override
+            public Optional<Member> delete(Integer id) {
+                return Optional.ofNullable(memberMap.remove(id));
+            }
+
+            @Override
+            public Optional<Member> save(Member member) {
+                return Optional.ofNullable(memberMap.put(member.getId(), member));
+            }
+        };
+        repository.init();
+        return repository;
+    }
+}
---
+++ src/main/java/com/example/member/Member.java
@@ -0,0 +1,40 @@
+package com.example.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class Member {
+    private Integer id;
+    private String name;
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
