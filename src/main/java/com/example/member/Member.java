302cfc71f79a0b56f54eefe5d5a89405200cc925
Kuesung Park <gueit214@naver.com>
2023-10-10 14:20:42 +0900

    feat : Member에 Lombok의 Getter, Setter 추가

--- src/main/java/com/example/member/Member.java
+++ src/main/java/com/example/member/Member.java
@@ -1,40 +1,14 @@
 package com.example.member;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@ToString
+@Getter // getter 메소드 자동 생성
+@Setter // setter 메소드 자동 생성
+@AllArgsConstructor // 모든 필드를 파라미터로 받는 생성자
+@ToString // toString 메소드 자동 생성
+@NoArgsConstructor // 기본 생성자
 public class Member {
     private Integer id;
     private String name;
     private String email;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
