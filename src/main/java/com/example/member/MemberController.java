c49b2d3ad97a3723d396af0a0067c4180e5e8db8
Kuesung Park <gueit214@naver.com>
2023-10-10 14:22:32 +0900

    feat : Member Controller 구현  - 목록 보기

--- src/main/java/com/example/member/MemberController.java
+++ src/main/java/com/example/member/MemberController.java
@@ -1,4 +1,23 @@
 package com.example.member;
 
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller // 컨트롤러임을 선언
+@RequiredArgsConstructor // final이 선언된 모든 필드를 인자값으로 하는 생성자를 대신 생성해줌
+@Slf4j  // 로그를 위한 어노테이션
+@RequestMapping("/member") // 기본적인 URL 매핑
 public class MemberController {
+  private final MemberRepository memberRepo;
+
+  @RequestMapping("/list")
+  @ResponseBody
+  public Iterable<Member> test2() {
+    return memberRepo.findAll();
+  }
+
 }
