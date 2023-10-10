d51a066b948e7cda0424c093b9d472175a8df9d5
Kuesung Park <gueit214@naver.com>
2023-10-10 15:02:19 +0900

    feat : list페이지 접속 시 member_list.html 반환

--- src/main/java/com/example/member/MemberController.java
+++ src/main/java/com/example/member/MemberController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // 컨트롤러임을 선언
 @RequiredArgsConstructor // final이 선언된 모든 필드를 인자값으로 하는 생성자를 대신 생성해줌
@@ -15,9 +14,9 @@ public class MemberController {
   private final MemberRepository memberRepo;
 
   @RequestMapping("/list")
-  @ResponseBody
-  public Iterable<Member> test2() {
-    return memberRepo.findAll();
+  public String test2() {
+    return "member/member_list"; // 경로 수정: "/" 제거
   }
-
 }
+
+
