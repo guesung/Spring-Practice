6f7ddc00062af8a03d8f7de24282db6c278c65fe
Kuesung Park <gueit214@naver.com>
2023-11-10 16:01:17 +0900

    Revert "refactor : view 관련 파일 제거"
    This reverts commit 36fce866494d9ec2ff362c9776d80967c72938a1.

---
+++ src/main/java/member/MemberController.java
@@ -0,0 +1,67 @@
+package member;
+
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller // 컨트롤러임을 선언
+@RequiredArgsConstructor // final이 선언된 모든 필드를 인자값으로 하는 생성자를 대신 생성해줌
+@Slf4j  // 로그를 위한 어노테이션
+@RequestMapping("member") // 기본적인 URL 매핑
+public class MemberController {
+  private final MemberRepository memberRepo;
+
+
+  @ModelAttribute
+  public void addAttributes(Model model) {
+    model.addAttribute("total", memberRepo.findAll().spliterator().getExactSizeIfKnown());
+  }
+
+  @GetMapping("list")
+  public String getMemberList(Model model) {
+    model.addAttribute("members", memberRepo.findAll());
+    return "member/member_list";
+  }
+
+  @GetMapping("form")
+  public String addMember(Model model) {
+    return "member/member_form";
+  }
+
+  @GetMapping("{id}")
+  public String getMemberById(@PathVariable int id, Model model) {
+    model.addAttribute("member", memberRepo.findById(id).orElse(null));
+    return "member/member_info";
+  }
+
+  @PostMapping("register")
+  public String registerMember(@Valid Member member, Errors errors, RedirectAttributes redirectAttributes) {
+    if (errors.hasErrors()) {
+      List<FieldError> list = errors.getFieldErrors();
+      list.forEach(e -> log.error(e.getDefaultMessage()));
+      redirectAttributes.addFlashAttribute("username", member.getUsername());
+      redirectAttributes.addFlashAttribute("email", member.getEmail());
+      redirectAttributes.addFlashAttribute("isError", true);
+      return "redirect:/member/form";
+    }
+    memberRepo.save(member);
+    return "redirect:/member/list";
+  }
+
+  @RequestMapping("delete/{id}")
+  public String deleteMember(@PathVariable int id) {
+    if (memberRepo.delete(id).isEmpty()) throw new RuntimeException("존재하는 아이디가 없습니다.");
+    return "redirect:/member/list";
+  }
+}
+
+
