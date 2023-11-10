43e0459a40d5cb61e275a89e226fc8f9752f8041
Kuesung Park <gueit214@naver.com>
2023-11-10 13:12:28 +0900

    feat : jpa로 구현

--- src/main/java/member/Member.java
+++ src/main/java/member/Member.java
@@ -1,22 +1,16 @@
 package member;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import lombok.*;
+import jakarta.persistence.*;
 
-@Getter // getter 메소드 자동 생성
-@Setter // setter 메소드 자동 생성
-@AllArgsConstructor // 모든 필드를 파라미터로 받는 생성자
-@ToString // toString 메소드 자동 생성
-@NoArgsConstructor // 기본 생성자
-@Data
+@Entity
 public class Member {
-  private Integer id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-  @NotBlank(message = "이름을 입력해주세요.")
+  @Column(nullable = false, unique = true)
   private String username;
 
-  @NotBlank(message = "이메일을 입력해주세요.")
-  @Email(message = "이메일 형식이 올바르지 않습니다.")
+  @Column(nullable = false, unique = true)
   private String email;
 }
--- src/main/java/member/MemberRepository.java
+++ src/main/java/member/MemberRepository.java
@@ -1,19 +1,6 @@
 package member;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository {
-  Map<Integer, Member> memberMap = new HashMap<>();
-
-  void init();
-
-  Optional<Member> findById(Integer id);
-
-  Iterable<Member> findAll();
-
-  Optional<Member> delete(Integer id);
-
-  Optional<Member> save(Member member);
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
