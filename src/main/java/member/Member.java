1731ab9b9ffde7f758b452f4256827a135aecd81
Kuesung Park <gueit214@naver.com>
2023-11-12 15:03:59 +0900

    feat : 엔티티 유효성 검사

--- src/main/java/member/Member.java
+++ src/main/java/member/Member.java
@@ -1,6 +1,8 @@
 package member;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -16,10 +18,13 @@ public class Member {
   @JoinColumn(name = "member_info_id")
   private MemberInfo memberInfo;
 
-  @
+  @NotBlank(message = "이름을 입력해주세요.")
+  @Email(message = "이름 형식이 올바르지 않습니다.")
   @Column(nullable = false, unique = true)
   private String username;
 
+  @NotBlank(message = "이메일을 입력해주세요.")
+  @Email(message = "이메일 형식이 올바르지 않습니다.")
   @Column(nullable = false, unique = true)
   private String email;
 }
--- src/main/java/member/MemberInfo.java
+++ src/main/java/member/MemberInfo.java
@@ -1,6 +1,7 @@
 package member;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,6 +16,7 @@ public class MemberInfo {
   @OneToOne(mappedBy = "memberInfo")
   private Member member; // Link to the Member entity
 
+  @NotBlank(message = "휴대폰 번호를 입력해주세요.")
   private String phoneNumber; // A unique number for the member
 
   @Enumerated(EnumType.STRING)
@@ -26,6 +28,6 @@ public class MemberInfo {
     기획자,
     마케터,
     기타
-    
+
   }
 }
